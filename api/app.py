56a7df30f110756f5ad64f7d236040c5a985e512
Yordi SE <yordanoslemmaworkofkind1@gmail.com>
2023-08-12 03:25:41 -0700

    api

--- api/app.py
+++ api/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    obj = storage.jop()
+    obj = storage.job()
 
     return render_template('home.html', obj=obj)
 
--- api/model/__init__.py
+++ api/model/__init__.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from model.engine.db_storage import DBStorage
 storage = DBStorage()
 storage.reload()
